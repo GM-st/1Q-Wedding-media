84c52c3e8e9d9da50a992184cce76901f344f14e
kim-byoungkwan <bg0307@ajou.ac.kr>
2022-09-17 22:22:01 +0900

    auth

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,14 +1,56 @@
+import mail from "@sendgrid/mail";
+import twilio from "twilio";
 import { NextApiRequest, NextApiResponse } from "next";
-import client from "../../../libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
+mail.setApiKey(process.env.SENDGRID_KEY!);
+const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
-export default async function handler(
-    req: NextApiRequest, 
-    res:NextApiResponse
-    ){
-    if(req.method !== "POST"){
-        res.status(401).end();
-    }
-    console.log(req.body.email)
-    res.status(200).end();
+async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<ResponseType>
+) {
+  const { phone, email } = req.body;
+  const user = phone ? { phone } : email ? { email } : null;
+  if (!user) return res.status(400).json({ ok: false });
+  const payload = Math.floor(100000 + Math.random() * 900000) + "";
+  const token = await client.token.create({
+    data: {
+      payload,
+      user: {
+        connectOrCreate: {
+          where: {
+            ...user,
+          },
+          create: {
+            name: "Anonymous",
+            ...user,
+          },
+        },
+      },
+    },
+  });
+  if (phone) {
+    /*  const message = await twilioClient.messages.create({
+      messagingServiceSid: process.env.TWILIO_MSID,
+      to: process.env.MY_PHONE!,
+      body: `Your login token is ${payload}.`,
+    });
+    console.log(message); */
+  } else if (email) {
+    /* const email = await mail.send({
+      from: "nico@nomadcoders.co",
+      to: "nico@nomadcoders.co",
+      subject: "Your Carrot Market Verification Email",
+      text: `Your token is ${payload}`,
+      html: `<strong>Your token is ${payload}</strong>`,
+    });
+    console.log(email); */
+  }
+  return res.json({
+    ok: true,
+  });
 }
+
+export default withHandler({ method: "POST", handler, isPrivate: false });
